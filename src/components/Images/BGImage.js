1b7306eddeeb9645fd559d38f0647c5ef642f4dd
@slovacus <slovacus@gmail.com>
2020-01-11 07:58:19 -0500

    (#5) feat: delete unnecesary components

--- src/components/Images/BGImage.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import { Image } from 'rebass';
-
-export const BGImage = ({ src }) => <Image src={src} />;
--- src/components/Images/ImageXL.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styled from 'styled-components';
-
-const IMG = styled.img`
-  transform: scale(2);
-`;
-
-export const ImageXL = ({ src }) => <IMG src={src} />;
